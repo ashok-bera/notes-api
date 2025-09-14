b447eef6625432385d231ddeb9287a09aa45aac2
ashok-bera <ashokkumarbera1999@gmail.com>
2025-09-14 10:18:35 +0530

    fixed test cases and added ci-cd pipeline config

--- src/test/java/org/example/note/NoteServiceTest.java
+++ src/test/java/org/example/note/NoteServiceTest.java
@@ -1,17 +1,21 @@
 package org.example.note;
 
-
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.example.dto.NoteRequest;
 import org.example.dto.NoteResponse;
 import org.example.model.Note;
 import org.example.model.User;
 import org.example.repo.NoteRepository;
+import org.example.repo.UserRepository;
 import org.example.service.NoteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +24,15 @@ class NoteServiceTest {
 
     @Mock
     private NoteRepository noteRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private MeterRegistry meterRegistry;
+
+    @Mock
+    private Counter counter;
 
     @InjectMocks
     private NoteService noteService;
@@ -31,13 +44,19 @@ class NoteServiceTest {
 
     @Test
     void shouldCreateNote() {
-        Note note = new Note();
         User user = new User();
         user.setEmail("manu@2000");
+
+        Note note = new Note();
         note.setUser(user);
         note.setTitle("new bro");
         note.setContent("wowo aweos ebro");
+
+        when(userRepository.findByEmail("manu@2000")).thenReturn(Optional.of(user));
         when(noteRepository.save(any(Note.class))).thenReturn(note);
+        when(meterRegistry.counter(anyString(), anyString(), anyString())).thenReturn(counter);
+
+        doNothing().when(counter).increment();
 
         NoteRequest noteRequest = new NoteRequest();
         noteRequest.setTitle("new bro");
@@ -45,9 +64,10 @@ class NoteServiceTest {
 
         NoteResponse savedNote = noteService.createNote("manu@2000", noteRequest);
 
-        assertEquals("Title", savedNote.getTitle());
+        assertEquals("new bro", savedNote.getTitle());
+
+        verify(userRepository, times(1)).findByEmail("manu@2000");
         verify(noteRepository, times(1)).save(any(Note.class));
+        verify(counter, times(1)).increment();
     }
-
 }
-
